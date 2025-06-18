c1dd544ac4b19d06364658a7e3f99198b05ae743
Kennedy Sovine <s.sovine@Me.com>
2025-06-18 11:34:00 +0100

    Update BIO and Remove Bulk

--- user-data/data.js
+++ user-data/data.js
@@ -1,6 +1,6 @@
 export const bio = [
-  "Since I was young, programming and working with computers has been my passion. I started taking summer courses at <strong>IDTech</strong> before taking <strong>AP Computer Science A</strong> in Highschool, scoring a <strong>5</strong> on the AP exam.",
-  "In university, I have moved on to take <strong>Computer Science for Games</strong>, developing many game prototypes and learning a lot about game development in the process.",
+  "University of Brighton student pursuing a BSc in Computer Science for Games, specializing in backend systems and plugin development for Minecraft servers. As a developer for a Minecraft server, contributed to gameplay enhancements and infrastructure design, collaborating closely with teams and content creators like Fundy to deliver engaging experiences.",
+  "Supported by a solid foundation in Java and C#, worked on scalable multiplayer systems and technical implementation. Passionate about solving complex problems, creating seamless player interactions, and driving innovation in game development environments.",
 ];
 
 export const skills = [
