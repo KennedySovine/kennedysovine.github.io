b396c4ef7c8cb21f418181bfa9461269767d7b28
Kennedy Sovine <s.sovine@Me.com>
2025-06-30 18:57:25 +0100

    Add new artwork: test

---
+++ user-data/art-data.js
@@ -0,0 +1,25 @@
+// Art portfolio data
+// This file is automatically updated by the admin panel
+
+const artData = [
+  {
+    "id": 1751306242969,
+    "title": "test",
+    "description": "",
+    "category": "digital",
+    "date": "2025-06",
+    "datePrecision": "month",
+    "formattedDate": "June 2025",
+    "tags": [
+      "hello"
+    ],
+    "linkedProject": null,
+    "image": "IMAGES/art/digital-UNNAMEDPROTAG-2025-06-30T17-57-22-969Z.png",
+    "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-UNNAMEDPROTAG-2025-06-30T17-57-22-969Z.png",
+    "uploadDate": "2025-06-30T17:57:24.502Z",
+    "featured": false
+  }
+];
+
+// Make data available globally for your portfolio to use
+window.artData = artData;
