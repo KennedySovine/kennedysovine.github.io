3a4f3b905057659b6ac796a6e226020fab9648b8
Kennedy Sovine <s.sovine@Me.com>
2025-06-30 23:23:36 +0100

    Add new artwork: Unnamed Protagonist Sprite Sheet

--- user-data/art-data.js
+++ user-data/art-data.js
@@ -27,6 +27,26 @@ const artData = [
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-UNNAMEDPROTAG-2025-06-30T22-21-06-427Z.png",
     "uploadDate": "2025-06-30T22:21:08.066Z",
     "featured": false
+  },
+  {
+    "id": 1751322214165,
+    "title": "Unnamed Protagonist Sprite Sheet",
+    "description": "Sprite Sheet for My Guy",
+    "category": "digital",
+    "date": "2025-06",
+    "datePrecision": "month",
+    "formattedDate": "June 2025",
+    "tags": [
+      "Prism of Light",
+      "greyscale",
+      "protag",
+      "procreate"
+    ],
+    "linkedProject": null,
+    "image": "IMAGES/art/digital-01d5f0952238d0ed8bd0176c08b8024583f8b3f476-2025-06-30T22-23-34-165Z.jpg",
+    "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-01d5f0952238d0ed8bd0176c08b8024583f8b3f476-2025-06-30T22-23-34-165Z.jpg",
+    "uploadDate": "2025-06-30T22:23:35.306Z",
+    "featured": false
   }
 ];
 
