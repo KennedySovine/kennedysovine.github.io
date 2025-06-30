68da2b0efcd1ea8f83380ce924dce058c41e1fed
Kennedy Sovine <s.sovine@Me.com>
2025-06-30 19:01:02 +0100

    Add new artwork: test

--- user-data/art-data.js
+++ user-data/art-data.js
@@ -18,6 +18,23 @@ const artData = [
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-UNNAMEDPROTAG-2025-06-30T17-57-22-969Z.png",
     "uploadDate": "2025-06-30T17:57:24.502Z",
     "featured": false
+  },
+  {
+    "id": 1751306459648,
+    "title": "test",
+    "description": "test",
+    "category": "digital",
+    "date": "2025-06",
+    "datePrecision": "month",
+    "formattedDate": "June 2025",
+    "tags": [
+      "hello"
+    ],
+    "linkedProject": null,
+    "image": "IMAGES/art/digital-UNNAMEDPROTAG-2025-06-30T18-00-59-648Z.png",
+    "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-UNNAMEDPROTAG-2025-06-30T18-00-59-648Z.png",
+    "uploadDate": "2025-06-30T18:01:01.243Z",
+    "featured": false
   }
 ];
 
