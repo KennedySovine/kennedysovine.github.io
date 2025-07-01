0c1a2635f7ecf7913f3a63bd68f1af38cac520ff
Kennedy Sovine <s.sovine@Me.com>
2025-07-01 03:02:18 +0100

    Update artwork data via admin panel

--- user-data/art-data.js
+++ user-data/art-data.js
@@ -3,54 +3,27 @@
 
 export const artworks = [
   {
-    "id": 1751322066427,
-    "title": "Unnamed Protagonist",
-    "description": "Art piece for a protagonist for a game I may one day make.",
+    "id": 1751335335596,
+    "title": "Chapter 2 End Deltarune",
+    "description": "That epic moment when Kris opens a dark fountain in their living room.",
     "category": "digital",
-    "date": "2025-06",
-    "datePrecision": "month",
-    "formattedDate": "June 2025",
+    "date": "2025-06-17",
+    "datePrecision": "full",
+    "formattedDate": "17 June 2025",
     "tags": [
-      "Concept Art",
-      "Greyscale",
-      "Procreate",
-      "Prism of Light"
+      "Fan art",
+      "procreate",
+      "Kris"
     ],
     "linkedProject": {
-      "title": "Prism of Light",
+      "title": "Deltarune",
       "type": "custom",
       "url": null,
       "language": null
     },
-    "image": "IMAGES/art/digital-UNNAMEDPROTAG-2025-06-30T22-21-06-427Z.png",
-    "imageUrl": "https://raw.githubusercontent.com/kennedysovine/kennedysovine.github.io/main/IMAGES/art/digital-UNNAMEDPROTAG-2025-06-30T22-21-06-427Z.png",
-    "uploadDate": "2025-06-30T22:21:08.066Z",
-    "featured": false
-  },
-  {
-    "id": 1751322214165,
-    "title": "Unnamed Protagonist Sprite Sheet",
-    "description": "Sprite Sheet for My Guy",
-    "category": "digital",
-    "date": "2025-06",
-    "datePrecision": "month",
-    "formattedDate": "June 2025",
-    "tags": [
-      "Prism of Light",
-      "greyscale",
-      "protag",
-      "procreate"
-    ],
-    "linkedProject": {
-      "title": "Prism of Light",
-      "type": "custom",
-      "url": null,
-      "language": null
-    },
-    "image": "IMAGES/art/digital-01d5f0952238d0ed8bd0176c08b8024583f8b3f476-2025-06-30T22-23-34-165Z.jpg",
-    "imageUrl": "https://raw.githubusercontent.com/kennedysovine/kennedysovine.github.io/main/IMAGES/art/digital-01d5f0952238d0ed8bd0176c08b8024583f8b3f476-2025-06-30T22-23-34-165Z.jpg",
-    "uploadDate": "2025-06-30T22:23:35.306Z",
-    "featured": false
+    "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-01faf317b201ade5b806a76ea3100ea7133677bec2-2025-07-01T02-02-15-596Z.jpg",
+    "imagePath": "IMAGES/art/digital-01faf317b201ade5b806a76ea3100ea7133677bec2-2025-07-01T02-02-15-596Z.jpg",
+    "uploadDate": "2025-07-01T02:02:16.825Z"
   }
 ];
 
