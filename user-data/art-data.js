90809538c3e6f0afc90b92377c47437bbc1bfc86
Kennedy Sovine <s.sovine@Me.com>
2025-07-01 03:03:17 +0100

    Update artwork data via admin panel

--- user-data/art-data.js
+++ user-data/art-data.js
@@ -24,6 +24,28 @@ export const artworks = [
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-01faf317b201ade5b806a76ea3100ea7133677bec2-2025-07-01T02-02-15-596Z.jpg",
     "imagePath": "IMAGES/art/digital-01faf317b201ade5b806a76ea3100ea7133677bec2-2025-07-01T02-02-15-596Z.jpg",
     "uploadDate": "2025-07-01T02:02:16.825Z"
+  },
+  {
+    "id": 1751335395046,
+    "title": "Fleur·Paraiba·Underhill",
+    "description": "https://artfight.net/attack/7232203.fleurparaibaunderhill",
+    "category": "digital",
+    "date": "2024-07-18",
+    "datePrecision": "full",
+    "formattedDate": "18 July 2024",
+    "tags": [
+      "Artfight 2024",
+      "procreate"
+    ],
+    "linkedProject": {
+      "title": "Art Fight",
+      "type": "custom",
+      "url": null,
+      "language": null
+    },
+    "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-01956813bc782037d15fadd38454d2a3a4727eef3e_00001-2025-07-01T02-03-15-046Z.jpg",
+    "imagePath": "IMAGES/art/digital-01956813bc782037d15fadd38454d2a3a4727eef3e_00001-2025-07-01T02-03-15-046Z.jpg",
+    "uploadDate": "2025-07-01T02:03:16.366Z"
   }
 ];
 
