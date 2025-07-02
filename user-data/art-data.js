368e1ede94448d886c4324e8ae461655ee64d3b4
Kennedy Sovine <s.sovine@Me.com>
2025-07-02 11:12:56 +0100

    Fixed Art Data formatting

--- user-data/art-data.js
+++ user-data/art-data.js
@@ -32,7 +32,7 @@ export const artworks = [
     "id": 1751322214165,
     "title": "Unnamed Protagonist Sprite Sheet",
     "description": "Sprite Sheet for My Guy",
-    "category": "digital",
+    "category": "Digital Art",
     "date": "2025-06",
     "datePrecision": "month",
     "formattedDate": "June 2025",
@@ -51,13 +51,14 @@ export const artworks = [
     "image": "IMAGES/art/digital-01d5f0952238d0ed8bd0176c08b8024583f8b3f476-2025-06-30T22-23-34-165Z.jpg",
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-01d5f0952238d0ed8bd0176c08b8024583f8b3f476-2025-06-30T22-23-34-165Z.jpg",
     "uploadDate": "2025-06-30T22:23:35.306Z",
-    "featured": false
+    "featured": false,
+    "year": "2025"
   },
   {
     "id": 1751335335596,
     "title": "Chapter 2 End Deltarune",
     "description": "That epic moment when Kris opens a dark fountain in their living room.",
-    "category": "digital",
+    "category": "Digital Art",
     "date": "2025-06-17",
     "datePrecision": "full",
     "formattedDate": "17 June 2025",
@@ -72,15 +73,17 @@ export const artworks = [
       "url": null,
       "language": null
     },
+    "image": "IMAGES/art/digital-01faf317b201ade5b806a76ea3100ea7133677bec2-2025-07-01T02-02-15-596Z.jpg",
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-01faf317b201ade5b806a76ea3100ea7133677bec2-2025-07-01T02-02-15-596Z.jpg",
-    "imagePath": "IMAGES/art/digital-01faf317b201ade5b806a76ea3100ea7133677bec2-2025-07-01T02-02-15-596Z.jpg",
-    "uploadDate": "2025-07-01T02:02:16.825Z"
+    "uploadDate": "2025-07-01T02:02:16.825Z",
+    "featured": false,
+    "year": "2025"
   },
   {
     "id": 1751335395046,
     "title": "Fleur:Paraiba:Underhill",
     "description": "https://artfight.net/attack/7232203.fleurparaibaunderhill",
-    "category": "digital",
+    "category": "Digital Art",
     "date": "2024-07-18",
     "datePrecision": "full",
     "formattedDate": "18 July 2024",
@@ -94,15 +97,17 @@ export const artworks = [
       "url": null,
       "language": null
     },
+    "image": "IMAGES/art/digital-01956813bc782037d15fadd38454d2a3a4727eef3e_00001-2025-07-01T02-03-15-046Z.jpg",
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-01956813bc782037d15fadd38454d2a3a4727eef3e_00001-2025-07-01T02-03-15-046Z.jpg",
-    "imagePath": "IMAGES/art/digital-01956813bc782037d15fadd38454d2a3a4727eef3e_00001-2025-07-01T02-03-15-046Z.jpg",
-    "uploadDate": "2025-07-01T02:03:16.366Z"
+    "uploadDate": "2025-07-01T02:03:16.366Z",
+    "featured": false,
+    "year": "2024"
   },
   {
     "id": 1751336278971,
     "title": "Technoblade Never Dies",
     "description": "",
-    "category": "digital",
+    "category": "Digital Art",
     "date": "2022-07-05",
     "datePrecision": "full",
     "formattedDate": "5 July 2022",
@@ -117,15 +122,17 @@ export const artworks = [
       "url": null,
       "language": null
     },
+    "image": "IMAGES/art/digital-01567b8f3973ba1a36055212b3c3aa50d0483b246c-2025-07-01T02-17-58-971Z.jpg",
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-01567b8f3973ba1a36055212b3c3aa50d0483b246c-2025-07-01T02-17-58-971Z.jpg",
-    "imagePath": "IMAGES/art/digital-01567b8f3973ba1a36055212b3c3aa50d0483b246c-2025-07-01T02-17-58-971Z.jpg",
-    "uploadDate": "2025-07-01T02:18:00.262Z"
+    "uploadDate": "2025-07-01T02:18:00.262Z",
+    "featured": false,
+    "year": "2022"
   },
   {
     "id": 1751336238849,
     "title": "Pantalone: Number 9 of the Fatui Harbingers",
     "description": "",
-    "category": "digital",
+    "category": "Digital Art",
     "date": "2022-07-28",
     "datePrecision": "full",
     "formattedDate": "28 July 2022",
@@ -134,15 +141,17 @@ export const artworks = [
       "Genshin Impact"
     ],
     "linkedProject": null,
+    "image": "IMAGES/art/digital-01af29f7243995c9284064c1462334b1ac7f3552ce-2025-07-01T02-17-18-849Z.jpg",
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-01af29f7243995c9284064c1462334b1ac7f3552ce-2025-07-01T02-17-18-849Z.jpg",
-    "imagePath": "IMAGES/art/digital-01af29f7243995c9284064c1462334b1ac7f3552ce-2025-07-01T02-17-18-849Z.jpg",
-    "uploadDate": "2025-07-01T02:17:20.222Z"
+    "uploadDate": "2025-07-01T02:17:20.222Z",
+    "featured": false,
+    "year": "2022"
   },
   {
     "id": 1751336155196,
     "title": "Gas Station in Utah",
     "description": "",
-    "category": "digital",
+    "category": "Digital Art",
     "date": "2022-09-04",
     "datePrecision": "full",
     "formattedDate": "4 September 2022",
@@ -156,15 +165,17 @@ export const artworks = [
       "url": null,
       "language": null
     },
+    "image": "IMAGES/art/digital-010afab06699ca171c1ea39d9e1424a0b501dfb767-2025-07-01T02-15-55-196Z.jpg",
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-010afab06699ca171c1ea39d9e1424a0b501dfb767-2025-07-01T02-15-55-196Z.jpg",
-    "imagePath": "IMAGES/art/digital-010afab06699ca171c1ea39d9e1424a0b501dfb767-2025-07-01T02-15-55-196Z.jpg",
-    "uploadDate": "2025-07-01T02:15:56.681Z"
+    "uploadDate": "2025-07-01T02:15:56.681Z",
+    "featured": false,
+    "year": "2022"
   },
   {
     "id": 1751336193899,
     "title": "Ghostbur with Friend",
     "description": "",
-    "category": "digital",
+    "category": "Digital Art",
     "date": "2022-09-03",
     "datePrecision": "full",
     "formattedDate": "3 September 2022",
@@ -178,15 +189,17 @@ export const artworks = [
       "url": null,
       "language": null
     },
+    "image": "IMAGES/art/digital-012d2d00d5bc61e6d20637eb583f49219145e29e5a-2025-07-01T02-16-33-899Z.jpg",
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-012d2d00d5bc61e6d20637eb583f49219145e29e5a-2025-07-01T02-16-33-899Z.jpg",
-    "imagePath": "IMAGES/art/digital-012d2d00d5bc61e6d20637eb583f49219145e29e5a-2025-07-01T02-16-33-899Z.jpg",
-    "uploadDate": "2025-07-01T02:16:34.947Z"
+    "uploadDate": "2025-07-01T02:16:34.947Z",
+    "featured": false,
+    "year": "2022"
   },
   {
     "id": 1751353734092,
     "title": "TommyInnit - The Fallen Angel",
     "description": "Drew over the Fallen Angel drawing",
-    "category": "digital",
+    "category": "Digital Art",
     "date": "2022-01-17",
     "datePrecision": "full",
     "formattedDate": "17 January 2022",
@@ -201,15 +214,17 @@ export const artworks = [
       "url": null,
       "language": null
     },
+    "image": "IMAGES/art/digital-01ba8c5e6be8ac882c1e62263603718cad77d7d4b8-2025-07-01T07-08-49-873Z.jpg",
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-01ba8c5e6be8ac882c1e62263603718cad77d7d4b8-2025-07-01T07-08-49-873Z.jpg",
-    "imagePath": "IMAGES/art/digital-01ba8c5e6be8ac882c1e62263603718cad77d7d4b8-2025-07-01T07-08-49-873Z.jpg",
-    "uploadDate": "2025-07-01T07:08:50.960Z"
+    "uploadDate": "2025-07-01T07:08:50.960Z",
+    "featured": false,
+    "year": "2022"
   },
   {
     "id": 1751353540220,
     "title": "Apple",
     "description": "",
-    "category": "digital",
+    "category": "Digital Art",
     "date": "2022-05-01",
     "datePrecision": "full",
     "formattedDate": "1 May 2022",
@@ -224,15 +239,17 @@ export const artworks = [
       "url": null,
       "language": null
     },
+    "image": "IMAGES/art/digital-013ace323dcd8d03a0a3a9248847da90f8743dc83d-2025-07-01T07-05-33-500Z.jpg",
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-013ace323dcd8d03a0a3a9248847da90f8743dc83d-2025-07-01T07-05-33-500Z.jpg",
-    "imagePath": "IMAGES/art/digital-013ace323dcd8d03a0a3a9248847da90f8743dc83d-2025-07-01T07-05-33-500Z.jpg",
-    "uploadDate": "2025-07-01T07:05:34.709Z"
+    "uploadDate": "2025-07-01T07:05:34.709Z",
+    "featured": false,
+    "year": "2022"
   },
   {
     "id": 1751353619720,
     "title": "Drowning Under the Water",
     "description": "",
-    "category": "digital",
+    "category": "Digital Art",
     "date": "2022-03-10",
     "datePrecision": "full",
     "formattedDate": "10 March 2022",
@@ -246,15 +263,17 @@ export const artworks = [
       "url": null,
       "language": null
     },
+    "image": "IMAGES/art/digital-0120073264dba4a24a0aa0b332c2f8f5cd4b3b7217-2025-07-01T07-06-58-166Z.jpg",
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-0120073264dba4a24a0aa0b332c2f8f5cd4b3b7217-2025-07-01T07-06-58-166Z.jpg",
-    "imagePath": "IMAGES/art/digital-0120073264dba4a24a0aa0b332c2f8f5cd4b3b7217-2025-07-01T07-06-58-166Z.jpg",
-    "uploadDate": "2025-07-01T07:06:59.386Z"
+    "uploadDate": "2025-07-01T07:06:59.386Z",
+    "featured": false,
+    "year": "2022"
   },
   {
     "id": 1751354434456,
     "title": "GhostBoo",
     "description": "",
-    "category": "digital",
+    "category": "Digital Art",
     "date": "2021-12-01",
     "datePrecision": "full",
     "formattedDate": "1 December 2021",
@@ -269,15 +288,17 @@ export const artworks = [
       "url": null,
       "language": null
     },
+    "image": "IMAGES/art/digital-01b815bee9c3cf6bf6031bb839623e476226d2f64e-2025-07-01T07-20-34-408Z.jpg",
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-01b815bee9c3cf6bf6031bb839623e476226d2f64e-2025-07-01T07-20-34-408Z.jpg",
-    "imagePath": "IMAGES/art/digital-01b815bee9c3cf6bf6031bb839623e476226d2f64e-2025-07-01T07-20-34-408Z.jpg",
-    "uploadDate": "2025-07-01T07:20:35.699Z"
+    "uploadDate": "2025-07-01T07:20:35.699Z",
+    "featured": false,
+    "year": "2021"
   },
   {
     "id": 1751355980347,
     "title": "Falling Through L'Manberg",
     "description": "",
-    "category": "digital",
+    "category": "Digital Art",
     "date": "2021-11-15",
     "datePrecision": "full",
     "formattedDate": "15 November 2021",
@@ -292,15 +313,17 @@ export const artworks = [
       "url": null,
       "language": null
     },
+    "image": "IMAGES/art/digital-0170b1b5c784e1a08eeab0fcbe5dedcb8e8e8677fa-2025-07-01T07-46-17-960Z.jpg",
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-0170b1b5c784e1a08eeab0fcbe5dedcb8e8e8677fa-2025-07-01T07-46-17-960Z.jpg",
-    "imagePath": "IMAGES/art/digital-0170b1b5c784e1a08eeab0fcbe5dedcb8e8e8677fa-2025-07-01T07-46-17-960Z.jpg",
-    "uploadDate": "2025-07-01T07:46:18.990Z"
+    "uploadDate": "2025-07-01T07:46:18.990Z",
+    "featured": false,
+    "year": "2021"
   },
   {
     "id": 1751356218421,
     "title": "Sea Fairy Cookie",
     "description": "",
-    "category": "digital",
+    "category": "Digital Art",
     "date": "2021-11-11",
     "datePrecision": "full",
     "formattedDate": "11 November 2021",
@@ -309,32 +332,39 @@ export const artworks = [
       "CookieRun:Kingdom"
     ],
     "linkedProject": null,
+    "image": "IMAGES/art/digital-01cf4afb5689050681205953f0fcbfaabfabe5cc19-2025-07-01T07-50-13-361Z.jpg",
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/digital-01cf4afb5689050681205953f0fcbfaabfabe5cc19-2025-07-01T07-50-13-361Z.jpg",
-    "imagePath": "IMAGES/art/digital-01cf4afb5689050681205953f0fcbfaabfabe5cc19-2025-07-01T07-50-13-361Z.jpg",
-    "uploadDate": "2025-07-01T07:50:14.698Z"
+    "uploadDate": "2025-07-01T07:50:14.698Z",
+    "featured": false,
+    "year": "2021"
   },
   {
     "id": 1751381471837,
     "title": "MCC Rising Team - Rainbow Racoons",
     "description": "",
     "category": "Digital Art",
-    "date": "September 2021",
+    "date": "2021-09",
+    "datePrecision": "month",
+    "formattedDate": "September 2021",
     "tags": [
       "Minecraft",
       "SourSticks"
     ],
-    "dateCreated": "2025-07-01",
+    "linkedProject": null,
+    "image": "IMAGES/art/1751381470898-010e727e7cd9ce3e1003d88fceb551c359c667cd03.jpg",
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/1751381470898-010e727e7cd9ce3e1003d88fceb551c359c667cd03.jpg",
-    "imagePath": "IMAGES/art/1751381470898-010e727e7cd9ce3e1003d88fceb551c359c667cd03.jpg",
-    "fullImagePath": "c:\\Users\\thatc\\OneDrive\\Documents\\GitHub\\kennedysovine.github.io\\IMAGES\\art\\1751381470898-010e727e7cd9ce3e1003d88fceb551c359c667cd03.jpg",
-    "uploadDate": "2025-07-01T14:51:10.900Z"
+    "uploadDate": "2025-07-01T14:51:10.900Z",
+    "featured": false,
+    "year": "2021"
   },
   {
     "id": 1751385909533,
     "title": "I'm Not a Good Soul --- 2025 Redraw",
     "description": "Second redraw!",
     "category": "Digital Art",
-    "date": "1 July 2025",
+    "date": "2025-07-01",
+    "datePrecision": "full",
+    "formattedDate": "1 July 2025",
     "tags": [
       "Undertale",
       "Fan art"
@@ -343,21 +373,22 @@ export const artworks = [
       "title": "Undertale",
       "type": "custom",
       "url": null,
-      "language": null,
-      "description": "Custom project"
+      "language": null
     },
-    "dateCreated": "2025-07-01",
+    "image": "IMAGES/art/1751385908904-014a7d9992ac3eff19069b8ffe66206d9eec6b2199.jpg",
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/1751385908904-014a7d9992ac3eff19069b8ffe66206d9eec6b2199.jpg",
-    "imagePath": "IMAGES/art/1751385908904-014a7d9992ac3eff19069b8ffe66206d9eec6b2199.jpg",
-    "fullImagePath": "c:\\Users\\thatc\\OneDrive\\Documents\\GitHub\\kennedysovine.github.io\\IMAGES\\art\\1751385908904-014a7d9992ac3eff19069b8ffe66206d9eec6b2199.jpg",
-    "uploadDate": "2025-07-01T16:05:08.906Z"
+    "uploadDate": "2025-07-01T16:05:08.906Z",
+    "featured": false,
+    "year": "2025"
   },
   {
     "id": 1751386316041,
     "title": "Snowdin Tubbo",
     "description": "",
     "category": "Digital Art",
-    "date": "1 August 2025",
+    "date": "2025-08-01",
+    "datePrecision": "full",
+    "formattedDate": "1 August 2025",
     "tags": [
       "DreamSMP",
       "Fan art",
@@ -367,39 +398,42 @@ export const artworks = [
       "title": "Dream SMP",
       "type": "custom",
       "url": null,
-      "language": null,
-      "description": "Fan art collection"
+      "language": null
     },
-    "dateCreated": "2025-07-01",
+    "image": "IMAGES/art/1751386315591-01232f7491a553e71cfe8a2ebdc3fb2eacd132c6d5.jpg",
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/1751386315591-01232f7491a553e71cfe8a2ebdc3fb2eacd132c6d5.jpg",
-    "imagePath": "IMAGES/art/1751386315591-01232f7491a553e71cfe8a2ebdc3fb2eacd132c6d5.jpg",
-    "fullImagePath": "c:\\Users\\thatc\\OneDrive\\Documents\\GitHub\\kennedysovine.github.io\\IMAGES\\art\\1751386315591-01232f7491a553e71cfe8a2ebdc3fb2eacd132c6d5.jpg",
-    "uploadDate": "2025-07-01T16:11:55.592Z"
+    "uploadDate": "2025-07-01T16:11:55.592Z",
+    "featured": false,
+    "year": "2025"
   },
   {
     "id": 1751386381016,
     "title": "Cecilia Character Sheet",
     "description": "Minecraft OC",
     "category": "Digital Art",
-    "date": "7 June 2025",
+    "date": "2025-06-07",
+    "datePrecision": "full",
+    "formattedDate": "7 June 2025",
     "tags": [
       "Minecraft",
       "OC",
       "Concept Art"
     ],
     "linkedProject": null,
-    "dateCreated": "2025-07-01",
+    "image": "IMAGES/art/1751386380103-01775fb3118723385b27345f521ebf3399ee796775.jpg",
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/1751386380103-01775fb3118723385b27345f521ebf3399ee796775.jpg",
-    "imagePath": "IMAGES/art/1751386380103-01775fb3118723385b27345f521ebf3399ee796775.jpg",
-    "fullImagePath": "c:\\Users\\thatc\\OneDrive\\Documents\\GitHub\\kennedysovine.github.io\\IMAGES\\art\\1751386380103-01775fb3118723385b27345f521ebf3399ee796775.jpg",
-    "uploadDate": "2025-07-01T16:13:00.107Z"
+    "uploadDate": "2025-07-01T16:13:00.107Z",
+    "featured": false,
+    "year": "2025"
   },
   {
     "id": 1751407444680,
     "title": "Abbie Crouching",
     "description": "Art fight attack! https://artfight.net/attack/8795573.abbie-crouching",
     "category": "Digital Art",
-    "date": "1 July 2025",
+    "date": "2025-07-01",
+    "datePrecision": "full",
+    "formattedDate": "1 July 2025",
     "tags": [
       "Artfight 2025"
     ],
@@ -407,14 +441,13 @@ export const artworks = [
       "title": "Art Fight",
       "type": "custom",
       "url": null,
-      "language": null,
-      "description": "Annual art challenge"
+      "language": null
     },
-    "dateCreated": "2025-07-01",
+    "image": "IMAGES/art/1751407444149-010e7ad4be06890c0717aea4861d8a553a2351e9d8.jpg",
     "imageUrl": "https://raw.githubusercontent.com/KennedySovine/kennedysovine.github.io/main/IMAGES/art/1751407444149-010e7ad4be06890c0717aea4861d8a553a2351e9d8.jpg",
-    "imagePath": "IMAGES/art/1751407444149-010e7ad4be06890c0717aea4861d8a553a2351e9d8.jpg",
-    "fullImagePath": "c:\\Users\\thatc\\OneDrive\\Documents\\GitHub\\kennedysovine.github.io\\IMAGES\\art\\1751407444149-010e7ad4be06890c0717aea4861d8a553a2351e9d8.jpg",
-    "uploadDate": "2025-07-01T22:04:04.150Z"
+    "uploadDate": "2025-07-01T22:04:04.150Z",
+    "featured": false,
+    "year": "2025"
   }
 ];
 
