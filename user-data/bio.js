b0d8d980ac39831dc73202963f5ffe522b03c64b
Kennedy Sovine <s.sovine@Me.com>
2025-07-09 11:47:45 +0100

    Update bio.js

--- user-data/bio.js
+++ user-data/bio.js
@@ -1,4 +1,8 @@
 export const bio = [
-  "University of Brighton student pursuing a BSc in Computer Science for Games, specializing in backend systems and plugin development for Minecraft servers. As a developer for a Minecraft server, contributed to gameplay enhancements and infrastructure design, collaborating closely with teams and content creators like Fundy to deliver engaging experiences.",
-  "Supported by a solid foundation in Java and C#, worked on scalable multiplayer systems and technical implementation. Passionate about solving complex problems, creating seamless player interactions, and driving innovation in game development environments."
+    "I am a recent graduate of the University of Brighton. I have recieved a Bachelor of Science (Honors) Computer Science for Games; Second Class Honors (Upper Division)",
+    "My education and knowledge is supported by a strong foundation in Java (AP Computer Science A) and C# (Unity Projects). I have worked in local multiplayer games and standard software engineering.",
+    "I am passionate about games such as League of Legends and Undertale. I have a wide range of interests in what I enjoy. I stream on Twitch showcasing my love and drive for the games I play as well.",
+    "I have worked in many team settings in my group projects at university, where I have taken the lead and also taken a back seat and let my group mates shine in what they wanted to do.",
+    "I am a quick learner and a hard worker. Whenever I have a chance, I enjoy learning new things and new techniques to solve problems.",
+    "I am looking for roles where I can learn and fix. Bug fixing and trouble shooting is one of my greatest skills alongside picking up new skills quickly." 
 ];
